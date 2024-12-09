64a693c9d32d874d95a73c5f3cafe502536d69f4
Artem Abramov <artemabramov230999@gmail.com>
2024-12-09 19:14:40 +0100

    desktop version cite

--- js/main.js
+++ js/main.js
@@ -9,7 +9,12 @@ $(function() {
   var mixer = mixitup('.portfolio__content');
 
   // Fancybox
-  Fancybox.bind('[data-fancybox]', {
-    //
-  });  
+  Fancybox.bind('[data-fancybox]', {});
+  
+  // Slick-slider
+  $('.blog-slider__inner').slick({
+    dots: true,
+    prevArrow: '<button class="slick-prev slick-arrow" type="button"><img src="images/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow: '<button class="slick-next slick-arrow" type="button"><img src="images/arrow-right.svg" alt="arrow-right"></button>',
+  });
 })
